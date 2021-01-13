4d7d880ad7b04d2603f5a76ac7ea3473a37c023c
Marnie Robinson <marnielane@gmail.com>
2021-01-13 19:07:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,14 +30,14 @@ def sorted_matrix
     ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], 
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
     ]
-  sorted_matrix = []
-  assembled_matrix[0].sort = sorted_matrix[0]
-  assembled_matrix[1].sort = sorted_matrix[1]
-  sorted_matrix
+  #sorted_matrix = []
+  #assembled_matrix[0].sort = sorted_matrix[0]
+  #assembled_matrix[1].sort = sorted_matrix[1]
+  #sorted_matrix
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
-
+  sorted_matrix = [(assembled_matrix[0].sort), (assembled_matrix[1])]
 end
 
 def matrix_lookup(matrix, row, column)
